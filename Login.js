70d91acc65bbbf05e4ec921d07a184ef1869715b
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-06-18 12:02:46 +0000

    Update Login.js
    changed the position of the whole page to flex start

--- Login.js
+++ Login.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-     alignItems: 'flex-start',
+ alignItems: 'flex-start',
     justifyContent: 'flex-start',
     padding: 16,
   },
