f9cfa448199ec8ccc5548cb7f1bff37d072669ba
Philomina Annan- 11347739 <pannan027@st.ug.edu.gh>
2024-06-18 11:33:02 +0000

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Button
+        title="Login"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 24,
+  },
+  input: {
+    width: '100%',
+    padding: 10,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+});
